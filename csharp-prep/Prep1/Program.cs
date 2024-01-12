25b884234ad6b9660ba2dc000392256ed9ab4d50
Egor Smirnov <exsplorator@gmail.com>
2024-01-12 11:04:34 -0700

    assignment stuff

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string fname;
+        fname = (Console.ReadLine());
+
+        Console.Write("What is your last name? ");
+        string lname;
+        lname = (Console.ReadLine());
+
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}");
+
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string percGrade;
+        string message;
+
+        while (true)
+        {
+            Console.WriteLine("Enter your % of a grade. Just numbers, no need for % sign");
+            percGrade = Console.ReadLine();
+
+            if (int.TryParse(percGrade, out int numGrade))
+            {
+                if (numGrade > 100){
+                    message = $"Are you sure you've got {percGrade}%? I doubt that";
+                    break;
+                }
+                else if (numGrade >= 90)
+                {
+                    message = $"You've got an A, you've got {percGrade}%";
+                    break;
+                }
+                else if (numGrade >= 80)
+                {
+                    message = $"You've got a B, you've got {percGrade}%";
+                    break;
+                }
+                else if (numGrade >= 70)
+                {
+                    message = $"You've got a C, you've got {percGrade}%";
+                    break;
+                }
+                
+                else
+                {
+                    message = $"Bruh, you've failed, you've got {percGrade}%";
+                    break;
+                };
+            }
+            else
+            {
+                Console.WriteLine("You entered something other than a number. Try again");
+            }
+        }
+        Console.WriteLine(message);
     }
 }
