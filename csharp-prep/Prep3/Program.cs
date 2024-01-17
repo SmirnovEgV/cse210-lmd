e4118c6c1a2fd1671e20b7d2f7900c1762ee08d7
Egor Smirnov <exsplorator@gmail.com>
2024-01-16 19:47:14 -0700

    guess withoug outputs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       Random randomGenerator = new Random();
+
+       int magicNum = randomGenerator.Next(1,101);
+
+       int guess = -1;
+
+       while (guess != magicNum){
+
+        Console.WriteLine("What is your guess? ");
+        guess = int.Parse(Console.ReadLine());
+
+       
+            if (magicNum > guess){
+
+                Console.WriteLine("Try bigger!");
+            }
+            else if (magicNum < guess){
+
+                Console.WriteLine("Try lower!");
+            }
+            else{
+
+                Console.WriteLine("You guessed it right");
+            }
+
+       }
+
     }
 }
