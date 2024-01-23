1826ee46669099484d246cc86bbcfaa9e0620d4c
Egor Smirnov <exsplorator@gmail.com>
2024-01-22 20:08:19 -0700

    working thing with abstractions.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Amazone";
+        job1._startYear = 2019;
+        job1._endYear = 2025;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2014;
+        job2._endYear = 2022;
+
+        Resume resume01 = new Resume();
+        resume01._name = "Egor Smirnov";
+        resume01._Jobs.Add(job1);
+        resume01._Jobs.Add(job2);
+
+        resume01.DisplayResume();
     }
 }
