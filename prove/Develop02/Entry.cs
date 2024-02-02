d0ee4afa2469da861a594fc7ec0879e268d8fd07
Egor Smirnov <exsplorator@gmail.com>
2024-02-01 21:21:06 -0700

    save/load crap done

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,7 +1,7 @@
 public class Entry{
-    public string _prompt;
-    public string _userInput;
-    public string _date;
+    public string _prompt {get; set;}
+    public string _userInput {get; set;}
+    public string _date {get; set;}
 
     public void DisplayEntry(){
         Console.WriteLine($"Date: {_date}\nPrompt: {_prompt}\nInput: {_userInput}");
--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,12 +1,11 @@
+using System.Text.Json;
 public class Journal{
 
-    
-    
     PromptGenerator promptGenerator;
-    public List<Entry> _entries = new List<Entry>();
-
+    public List<Entry> _entries { get; private set; } = new List<Entry>();
     public Journal()
     {
+        _entries = new List<Entry>();
         promptGenerator = new PromptGenerator();
     }
     
@@ -32,6 +31,7 @@ public class Journal{
     public void DisplayAllEntries(){
 
         Console.WriteLine("All entries:");
+        Console.WriteLine(_entries.Count);
 
         foreach (var _entry in _entries){
             _entry.DisplayEntry();            
@@ -39,10 +39,44 @@ public class Journal{
 
     }
     public void SaveToFile(){
+        Console.WriteLine("Enter the name of the file:");
+        string fileName = Console.ReadLine();
 
+        try 
+        {
+            string jsonString = JsonSerializer.Serialize(_entries);
+            File.WriteAllText(fileName + ".json", jsonString);
+            Console.WriteLine("Entries saved to JSON file successfully.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error saving to file: " + ex.Message);
+        }
     }
     public List<Entry> LoadFromFile(){
-        return _entries;
+
+        Console.Write("Enter the name of the JSON file to load: ");
+        string fileName = Console.ReadLine();
+
+        try
+        {
+            string jsonString = File.ReadAllText(fileName);
+            Console.WriteLine(jsonString);
+
+            _entries = JsonSerializer.Deserialize<List<Entry>>(jsonString);
+
+            Console.WriteLine(_entries.Count);
+
+
+            Console.WriteLine("Entries loaded from JSON file successfully.");
+
+            return _entries;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error loading from file: " + ex.Message);
+            return null; // Or return an empty list to handle the error gracefully
+        }
     }
 
 }
