b98b09122992e3c61cb77859712a0b77d782a846
Egor Smirnov <exsplorator@gmail.com>
2024-03-07 13:15:06 -0700

    aw you know, stuff

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        int choice;
+        do
+        {
+            Console.WriteLine($"You have 'points' points \n");
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("\t1. Create New Goal");
+            Console.WriteLine("\t2. List Goals");
+            Console.WriteLine("\t3. Save Goals");
+            Console.WriteLine("\t4. Load Goals");
+            Console.WriteLine("\t5. Record Event");
+            Console.WriteLine("\t6. Quit");
+
+            Console.Write("Select a choice from the menu: ");
+            if(int.TryParse(Console.ReadLine(), out choice))
+            {
+                switch(choice)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        Console.WriteLine("Exiting the application...");
+                        Thread.Sleep(2500);
+                        break;
+                    default:
+                        Console.WriteLine("\n Not an option. Choose from options above and input a number");
+                        break;
+
+                }
+
+            }
+
+        }
+        while(choice != 5);
     }
 }
