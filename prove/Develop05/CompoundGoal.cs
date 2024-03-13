bccf422b5d9f88e179e9a8bf735318e42e107c56
Egor Smirnov <exsplorator@gmail.com>
2024-03-13 15:46:16 -0600

    m

--- prove/Develop05/CompoundGoal.cs
+++ prove/Develop05/CompoundGoal.cs
@@ -1,20 +1,20 @@
 class CompoundGoal : Goal 
 {
     private bool _isDone;
-    private int _numberOfCompletions;
+    private int _requiredCompletions;
     private int _completionsCounter;
     private int _bonusPoints;
-    public CompoundGoal(string name, string description, int points, int numberOfCompletions, int bonusPoints) : base(name, description, points)
+    public CompoundGoal(string name, string description, int points, int requiredCompletions, int bonusPoints) : base(name, description, points)
     {
         _isDone = false;
-        _numberOfCompletions = numberOfCompletions;
+        _requiredCompletions = requiredCompletions;
         _bonusPoints = bonusPoints;
         _completionsCounter = 0;
         
     }
     public override void SetDone(Goal goal)
     {
-        if(_completionsCounter < _numberOfCompletions)
+        if(_completionsCounter < _requiredCompletions)
         {
             _completionsCounter++ ; 
         }
@@ -24,21 +24,20 @@ class CompoundGoal : Goal
         }
 
     }
-    public override CompoundGoal CreateGoal()
+    public static CompoundGoal CreateGoalFromUserInput()
     {
         Console.WriteLine("Input the name for a Compound Goal");
         string name = Console.ReadLine();
         Console.WriteLine("Input the description for a Compound Goal");
         string description = Console.ReadLine();
         Console.WriteLine("How many times will it take to complete this goal?");
-        int numberOfCompletions = int.Parse(Console.ReadLine()); 
+        int requiredCompletions = int.Parse(Console.ReadLine());
         Console.WriteLine("How many points would you want on each completion");
         int points = int.Parse(Console.ReadLine());
-        Console.WriteLine("What amound of bonus poits would you like on final completion of a goal?");
+        Console.WriteLine("What amount of bonus points would you like on the final completion of a goal?");
         int bonusPoints = int.Parse(Console.ReadLine());
 
-        CompoundGoal compoundGoal = new CompoundGoal(name,description,points,numberOfCompletions,bonusPoints);
-        return compoundGoal;
+        return new CompoundGoal(name, description, points, requiredCompletions, bonusPoints);
     }
     public override int GetPoints(Goal goal)
     {
